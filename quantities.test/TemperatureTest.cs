597fdd42158621a4670034101da17c6ddadedfc3
Thomas Kägi <kaegit@gmail.com>
2023-05-01 13:29:48 +0200

    Use "Of" and "To" members on temperature.

--- quantities.test/TemperatureTest.cs
+++ quantities.test/TemperatureTest.cs
@@ -1,5 +1,7 @@
 ﻿using Quantities.Units.Imperial.Temperature;
 using Quantities.Units.NonStandard.Temperature;
+using Quantities.Units.Si.Derived;
+using Newton = Quantities.Units.NonStandard.Temperature.Newton;
 
 namespace Quantities.Test;
 
@@ -7,30 +9,30 @@ namespace Quantities.Test;
 public sealed class TemperatureTest
 {
     [Fact]
-    public void KelvinToString() => FormattingMatches(v => Temperature.Si<Kelvin>(v), "K");
+    public void KelvinToString() => FormattingMatches(v => Temperature.Of(v).Si<Kelvin>(), "K");
     [Fact]
-    public void CelsiusToString() => FormattingMatches(v => Temperature.Celsius(v), "°C");
+    public void CelsiusToString() => FormattingMatches(v => Temperature.Of(v).Metric<Celsius>(), "°C");
     [Fact]
-    public void DelisleToString() => FormattingMatches(v => Temperature.NonStandard<Delisle>(v), "°De");
+    public void DelisleToString() => FormattingMatches(v => Temperature.Of(v).NonStandard<Delisle>(), "°De");
     [Fact]
-    public void NewtonToString() => FormattingMatches(v => Temperature.NonStandard<Newton>(v), "°N");
+    public void NewtonToString() => FormattingMatches(v => Temperature.Of(v).NonStandard<Newton>(), "°N");
     [Fact]
-    public void RéaumurToString() => FormattingMatches(v => Temperature.NonStandard<Réaumur>(v), "°Ré");
+    public void RéaumurToString() => FormattingMatches(v => Temperature.Of(v).NonStandard<Réaumur>(), "°Ré");
     [Fact]
-    public void RømerToString() => FormattingMatches(v => Temperature.NonStandard<Rømer>(v), "°Rø");
+    public void RømerToString() => FormattingMatches(v => Temperature.Of(v).NonStandard<Rømer>(), "°Rø");
     [Fact]
-    public void FahrenheitToString() => FormattingMatches(v => Temperature.Imperial<Fahrenheit>(v), "°F");
+    public void FahrenheitToString() => FormattingMatches(v => Temperature.Of(v).Imperial<Fahrenheit>(), "°F");
     [Fact]
-    public void RankineToString() => FormattingMatches(v => Temperature.Imperial<Rankine>(v), "°R");
+    public void RankineToString() => FormattingMatches(v => Temperature.Of(v).Imperial<Rankine>(), "°R");
     [Fact]
-    public void GasMarkToString() => FormattingMatches(v => Temperature.Imperial<GasMark>(v), "GM");
+    public void GasMarkToString() => FormattingMatches(v => Temperature.Of(v).Imperial<GasMark>(), "GM");
     [Fact]
     public void AddThreeTemperatures()
     {
-        Temperature a = Temperature.Celsius(-40);
-        Temperature b = Temperature.Imperial<Fahrenheit>(-40);
-        Temperature c = Temperature.Si<Kelvin>(363.15);
-        Temperature expected = Temperature.Celsius(10);
+        Temperature a = Temperature.Of(-40).Metric<Celsius>();
+        Temperature b = Temperature.Of(-40).Imperial<Fahrenheit>();
+        Temperature c = Temperature.Of(363.15).Si<Kelvin>();
+        Temperature expected = Temperature.Of(10).Metric<Celsius>();
 
         Temperature actual = a + b + c;
 
@@ -39,9 +41,9 @@ public sealed class TemperatureTest
     [Fact]
     public void SubtractTemperatures()
     {
-        Temperature a = Temperature.Celsius(70);
-        Temperature b = Temperature.Si<Kelvin>(273.15 + 28);
-        Temperature expected = Temperature.Celsius(42);
+        Temperature a = Temperature.Of(70).Metric<Celsius>();
+        Temperature b = Temperature.Of(273.15 + 28).Si<Kelvin>();
+        Temperature expected = Temperature.Of(42).Metric<Celsius>();
 
         Temperature actual = a - b;
 
@@ -51,70 +53,70 @@ public sealed class TemperatureTest
     [Fact]
     public void KelvinToCelsius()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(36 + 273.15);
-        Temperature expected = Temperature.Celsius(36);
+        Temperature temperature = Temperature.Of(36 + 273.15).Si<Kelvin>();
+        Temperature expected = Temperature.Of(36).Metric<Celsius>();
 
-        Temperature actual = temperature.ToCelsius();
+        Temperature actual = temperature.To.Metric<Celsius>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToFahrenheit()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(364);
-        Temperature expected = Temperature.Imperial<Fahrenheit>(195.53);
+        Temperature temperature = Temperature.Of(364).Si<Kelvin>();
+        Temperature expected = Temperature.Of(195.53).Imperial<Fahrenheit>();
 
-        Temperature actual = temperature.ToImperial<Fahrenheit>();
+        Temperature actual = temperature.To.Imperial<Fahrenheit>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void CelsiusToKelvin()
     {
-        Temperature temperature = Temperature.Celsius(312 - 273.15);
-        Temperature expected = Temperature.Si<Kelvin>(312);
+        Temperature temperature = Temperature.Of(312 - 273.15).Metric<Celsius>();
+        Temperature expected = Temperature.Of(312).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void CelsiusToMilliKelvin()
     {
-        Temperature temperature = Temperature.Celsius(-273.13534324);
-        Temperature expected = Temperature.Si<Milli, Kelvin>(14.65676);
+        Temperature temperature = Temperature.Of(-273.13534324).Metric<Celsius>();
+        Temperature expected = Temperature.Of(14.65676).Si<Milli, Kelvin>();
 
-        Temperature actual = temperature.To<Milli, Kelvin>();
+        Temperature actual = temperature.To.Si<Milli, Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void CelsiusToFahrenheit()
     {
-        Temperature temperature = Temperature.Celsius(37.0);
-        Temperature expected = Temperature.Imperial<Fahrenheit>(98.6);
+        Temperature temperature = Temperature.Of(37.0).Metric<Celsius>();
+        Temperature expected = Temperature.Of(98.6).Imperial<Fahrenheit>();
 
-        Temperature actual = temperature.ToImperial<Fahrenheit>();
+        Temperature actual = temperature.To.Imperial<Fahrenheit>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void FahrenheitToKelvin()
     {
-        Temperature temperature = Temperature.Imperial<Fahrenheit>(-40);
-        Temperature expected = Temperature.Si<Kelvin>(233.15);
+        Temperature temperature = Temperature.Of(-40).Imperial<Fahrenheit>();
+        Temperature expected = Temperature.Of(233.15).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void CelsiusToGasMark()
     {
-        Temperature temperature = Temperature.Celsius(218d + (1d / 3d));
-        Temperature expected = Temperature.Imperial<GasMark>(7);
+        Temperature temperature = Temperature.Of(218d + (1d / 3d)).Metric<Celsius>();
+        Temperature expected = Temperature.Of(7).Imperial<GasMark>();
 
-        Temperature actual = temperature.ToImperial<GasMark>();
+        Temperature actual = temperature.To.Imperial<GasMark>();
 
         actual.Matches(expected, VeryLowPrecision);
     }
@@ -122,150 +124,150 @@ public sealed class TemperatureTest
     [Fact]
     public void FahrenheitToGasMark()
     {
-        Temperature temperature = Temperature.Imperial<Fahrenheit>(350);
-        Temperature expected = Temperature.Imperial<GasMark>(4);
+        Temperature temperature = Temperature.Of(350).Imperial<Fahrenheit>();
+        Temperature expected = Temperature.Of(4).Imperial<GasMark>();
 
-        Temperature actual = temperature.ToImperial<GasMark>();
+        Temperature actual = temperature.To.Imperial<GasMark>();
 
         actual.Matches(expected, VeryLowPrecision);
     }
     [Fact]
     public void GasMarkToFahrenheit()
     {
-        Temperature temperature = Temperature.Imperial<GasMark>(1);
-        Temperature expected = Temperature.Imperial<Fahrenheit>(275);
+        Temperature temperature = Temperature.Of(1).Imperial<GasMark>();
+        Temperature expected = Temperature.Of(275).Imperial<Fahrenheit>();
 
-        Temperature actual = temperature.ToImperial<Fahrenheit>();
+        Temperature actual = temperature.To.Imperial<Fahrenheit>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void GasMarkToKelvin()
     {
-        Temperature temperature = Temperature.Imperial<GasMark>(1);
-        Temperature expected = Temperature.Si<Kelvin>(408.15);
+        Temperature temperature = Temperature.Of(1).Imperial<GasMark>();
+        Temperature expected = Temperature.Of(408.15).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToGasMark()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(475);
-        Temperature expected = Temperature.Imperial<GasMark>(5.8132);
+        Temperature temperature = Temperature.Of(475).Si<Kelvin>();
+        Temperature expected = Temperature.Of(5.8132).Imperial<GasMark>();
 
-        Temperature actual = temperature.ToImperial<GasMark>();
+        Temperature actual = temperature.To.Imperial<GasMark>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToRankine()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(255.37);
-        Temperature expected = Temperature.Imperial<Rankine>(459.666);
+        Temperature temperature = Temperature.Of(255.37).Si<Kelvin>();
+        Temperature expected = Temperature.Of(459.666).Imperial<Rankine>();
 
-        Temperature actual = temperature.ToImperial<Rankine>();
+        Temperature actual = temperature.To.Imperial<Rankine>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void RankineToKelvin()
     {
-        Temperature temperature = Temperature.Imperial<Rankine>(671.64102);
-        Temperature expected = Temperature.Si<Kelvin>(373.1339);
+        Temperature temperature = Temperature.Of(671.64102).Imperial<Rankine>();
+        Temperature expected = Temperature.Of(373.1339).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void FahrenheitToRankine()
     {
-        Temperature temperature = Temperature.Imperial<Fahrenheit>(32);
-        Temperature expected = Temperature.Imperial<Rankine>(491.67);
+        Temperature temperature = Temperature.Of(32).Imperial<Fahrenheit>();
+        Temperature expected = Temperature.Of(491.67).Imperial<Rankine>();
 
-        Temperature actual = temperature.ToImperial<Rankine>();
+        Temperature actual = temperature.To.Imperial<Rankine>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToDelisle()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(273.16);
-        Temperature expected = Temperature.NonStandard<Delisle>(149.985);
+        Temperature temperature = Temperature.Of(273.16).Si<Kelvin>();
+        Temperature expected = Temperature.Of(149.985).NonStandard<Delisle>();
 
-        Temperature actual = temperature.ToNonStandard<Delisle>();
+        Temperature actual = temperature.To.NonStandard<Delisle>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void DelisleToKelvin()
     {
-        Temperature temperature = Temperature.NonStandard<Delisle>(176.67);
-        Temperature expected = Temperature.Si<Kelvin>(255.37);
+        Temperature temperature = Temperature.Of(176.67).NonStandard<Delisle>();
+        Temperature expected = Temperature.Of(255.37).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToNewton()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(373.15);
-        Temperature expected = Temperature.NonStandard<Newton>(33);
+        Temperature temperature = Temperature.Of(373.15).Si<Kelvin>();
+        Temperature expected = Temperature.Of(33).NonStandard<Newton>();
 
-        Temperature actual = temperature.ToNonStandard<Newton>();
+        Temperature actual = temperature.To.NonStandard<Newton>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void NewtonToKelvin()
     {
-        Temperature temperature = Temperature.NonStandard<Newton>(0);
-        Temperature expected = Temperature.Si<Kelvin>(273.15);
+        Temperature temperature = Temperature.Of(0).NonStandard<Newton>();
+        Temperature expected = Temperature.Of(273.15).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToRéaumur()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(1941);
-        Temperature expected = Temperature.NonStandard<Réaumur>(1334.28);
+        Temperature temperature = Temperature.Of(1941).Si<Kelvin>();
+        Temperature expected = Temperature.Of(1334.28).NonStandard<Réaumur>();
 
-        Temperature actual = temperature.ToNonStandard<Réaumur>();
+        Temperature actual = temperature.To.NonStandard<Réaumur>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void RéaumurToKelvin()
     {
-        Temperature temperature = Temperature.NonStandard<Réaumur>(-14.22);
-        Temperature expected = Temperature.Si<Kelvin>(255.375);
+        Temperature temperature = Temperature.Of(-14.22).NonStandard<Réaumur>();
+        Temperature expected = Temperature.Of(255.375).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void KelvinToRømer()
     {
-        Temperature temperature = Temperature.Si<Kelvin>(255.37);
-        Temperature expected = Temperature.NonStandard<Rømer>(-1.8345);
+        Temperature temperature = Temperature.Of(255.37).Si<Kelvin>();
+        Temperature expected = Temperature.Of(-1.8345).NonStandard<Rømer>();
 
-        Temperature actual = temperature.ToNonStandard<Rømer>();
+        Temperature actual = temperature.To.NonStandard<Rømer>();
 
         actual.Matches(expected);
     }
     [Fact]
     public void RømerToKelvin()
     {
-        Temperature temperature = Temperature.NonStandard<Rømer>(7.50525);
-        Temperature expected = Temperature.Si<Kelvin>(273.16);
+        Temperature temperature = Temperature.Of(7.50525).NonStandard<Rømer>();
+        Temperature expected = Temperature.Of(273.16).Si<Kelvin>();
 
-        Temperature actual = temperature.To<Kelvin>();
+        Temperature actual = temperature.To.Si<Kelvin>();
 
         actual.Matches(expected);
     }

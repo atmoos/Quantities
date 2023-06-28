4d35e046e01a3200b752245606bb44da4bbc4074
Thomas Kägi <kaegit@gmail.com>
2023-06-28 16:28:50 +0200

    Add comparison benchmark Text.Json vs Newtonsoft.

--- quantities.benchmark/Convenience.cs
+++ quantities.benchmark/Convenience.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Text.Json;
+using Newtonsoft.Json;
 using Quantities.Serialization.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Quantities.Benchmark;
 
@@ -9,4 +11,5 @@ public static class Convenience
     private static readonly JsonSerializerOptions options = new JsonSerializerOptions().EnableQuantities();
     public static String Serialize<T>(this T value) => JsonSerializer.Serialize(value, options);
     public static T Deserialize<T>(this String value) => JsonSerializer.Deserialize<T>(value, options) ?? throw new Exception($"Deserialization of {typeof(T).Name} failed");
+    public static T Deserialize<T>(this String value, JsonSerializerSettings settings) => JsonConvert.DeserializeObject<T>(value, settings) ?? throw new Exception($"Deserialization of {typeof(T).Name} failed");
 }

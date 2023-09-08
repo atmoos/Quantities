32cd5310e513abe04f8d18c76a2e34a42d38cb8a
Thomas Kägi <kaegit@gmail.com>
2023-09-08 14:27:08 +0200

    Remove unused measure interfaces.

--- quantities/measures/IMeasure.cs
+++ quantities/measures/IMeasure.cs
@@ -1,37 +1,8 @@
 ﻿using Quantities.Numerics;
-using Quantities.Units.Imperial;
-using Quantities.Units.NonStandard;
-using Quantities.Units.Si;
 
 namespace Quantities.Measures;
 
 internal interface IMeasure : IRepresentable, ISerialize
 {
     public static abstract Polynomial Poly { get; }
-}
-internal interface ISiMeasure<in TUnit> : IMeasure
-    where TUnit : ISiUnit
-{
-    /* marker interface*/
-}
-/// <summary>
-/// SI accepts certain units for use in the SI system. However they are not regarded as actual SI units. 
-/// </summary>
-/// <remarks>
-/// An example of this is the litre which is defined in terms of the metre as: 1000 ℓ in 1 m³
-/// </remarks>
-internal interface IMetricMeasure<in TUnit> : IMeasure
-    where TUnit : IMetricUnit
-{
-    /* marker interface*/
-}
-internal interface IImperialMeasure<in TUnit> : IMeasure
-    where TUnit : IImperialUnit, ITransform, IRepresentable
-{
-    /* marker interface*/
-}
-internal interface INonStandardMeasure<in TUnit> : IMeasure
-    where TUnit : INoSystemUnit, ITransform, IRepresentable
-{
-    /* marker interface*/
 }

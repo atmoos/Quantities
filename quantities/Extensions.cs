3025b132e95816b82f5ed9062abf05c8e1a3be38
Thomas Kägi <kaegit@gmail.com>
2023-09-23 04:02:31 +0200

    Pretty print equal messages.

--- quantities/Extensions.cs
+++ quantities/Extensions.cs
@@ -9,14 +9,14 @@ namespace Quantities;
 
 public static class Extensions
 {
+    internal static String RoundTripFormat = "G17"; // https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#RFormatString
     internal static Double ValueOf<T>() where T : ITransform => Polynomial.Of<T>() * 1d;
     internal static Transformation RootedIn<TSi>(this Transformation self) where TSi : ISiUnit => self;
     internal static Transformation From<TBasis>(this Transformation self) where TBasis : IPrefix => TBasis.ToSi(self);
     internal static Transformation DerivedFrom<TBasis>(this Transformation self) where TBasis : IUnit, ITransform => TBasis.ToSi(self);
     public static String ToString(this IFormattable formattable, String format) => formattable.ToString(format, CultureInfo.InvariantCulture);
     internal static Quantity To<TMeasure>(this in Double value)
-      where TMeasure : IMeasure
-      => Quantity.Of<TMeasure>(in value);
+      where TMeasure : IMeasure => Quantity.Of<TMeasure>(in value);
     public static void Serialize<TQuantity>(this IQuantity<TQuantity> quantity, IWriter writer)
       where TQuantity : struct, IQuantity<TQuantity>, Dimensions.IDimension
     {

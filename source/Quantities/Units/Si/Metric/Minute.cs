401cad7018e5597090c0d5c82c7596d898d41860
Thomas Kägi <kaegit@gmail.com>
2023-09-30 10:49:22 +0200

    Separate out metric time units into seperate files.

--- source/Quantities/Units/Si/Metric/Minute.cs
+++ source/Quantities/Units/Si/Metric/Minute.cs
@@ -7,8 +7,3 @@ public readonly struct Minute : IMetricUnit, ITime
     public static Transformation ToSi(Transformation self) => 60 * self.RootedIn<Second>();
     public static String Representation => "m";
 }
-public readonly struct Hour : IMetricUnit, ITime
-{
-    public static Transformation ToSi(Transformation self) => 60 * self.DerivedFrom<Minute>();
-    public static String Representation => "h";
-}

33431965e39a91d3f8bbaa40f304abfd5fefd71c
Thomas Kägi <kaegit@gmail.com>
2023-11-25 13:23:51 +0100

    Update the performance comparisons.

--- source/Quantities.Benchmark/Compare/ScalarCreation.cs
+++ source/Quantities.Benchmark/Compare/ScalarCreation.cs
@@ -1,4 +1,3 @@
-using BenchmarkDotNet.Attributes;
 using Quantities.Prefixes;
 using Quantities.Units.Si;
 
@@ -12,7 +11,7 @@ public class ScalarCreation
     private static readonly Double value = Math.E;
 
     [Benchmark(Baseline = true)]
-    public Length Quantity() => Length.Of(value).Si<Centi, Metre>();
+    public Length Quantity() => Length.Of(value, Si<Centi, Metre>());
 
     [Benchmark]
     public nLength UnitsNet() => nLength.FromCentimeters(value);
@@ -23,13 +22,13 @@ public class ScalarCreation
 
 BenchmarkDotNet v0.13.8, Arch Linux
 Intel Core i7-8565U CPU 1.80GHz (Whiskey Lake), 1 CPU, 8 logical and 4 physical cores
-.NET SDK 7.0.111
-  [Host]     : .NET 7.0.11 (7.0.1123.46301), X64 RyuJIT AVX2
-  DefaultJob : .NET 7.0.11 (7.0.1123.46301), X64 RyuJIT AVX2
+.NET SDK 7.0.113
+  [Host]     : .NET 7.0.13 (7.0.1323.52501), X64 RyuJIT AVX2
+  DefaultJob : .NET 7.0.13 (7.0.1323.52501), X64 RyuJIT AVX2
 
 
-| Method   | Mean     | Error    | StdDev   | Ratio | RatioSD | Allocated | Alloc Ratio |
-|--------- |---------:|---------:|---------:|------:|--------:|----------:|------------:|
-| Quantity | 10.68 ns | 0.150 ns | 0.141 ns |  1.00 |    0.00 |         - |          NA |
-| UnitsNet | 12.84 ns | 0.139 ns | 0.130 ns |  1.20 |    0.02 |         - |          NA |
+| Method   | Mean      | Error     | StdDev    | Ratio | RatioSD | Allocated | Alloc Ratio |
+|--------- |----------:|----------:|----------:|------:|--------:|----------:|------------:|
+| Quantity |  4.877 ns | 0.0473 ns | 0.0395 ns |  1.00 |    0.00 |         - |          NA |
+| UnitsNet | 12.723 ns | 0.2395 ns | 0.2240 ns |  2.62 |    0.05 |         - |          NA |
 */

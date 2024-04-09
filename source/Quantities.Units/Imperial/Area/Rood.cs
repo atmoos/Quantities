f5cf758d15fdbb17b8a85f1607084975babb6582
Thomas Kägi <kaegit@gmail.com>
2024-04-09 22:11:35 +0200

    Bring implementation in dependent projects back.

--- source/Quantities.Units/Imperial/Area/Rood.cs
+++ source/Quantities.Units/Imperial/Area/Rood.cs
@@ -3,9 +3,9 @@ using Quantities.Units.Imperial.Length;
 
 namespace Quantities.Units.Imperial.Area;
 
-public readonly struct Rood : IImperialUnit, IArea, IAlias<ILength>
+public readonly struct Rood : IImperialUnit, IArea, IPowerOf<ILength>
 {
     public static Transformation ToSi(Transformation self) => 1011.7141056 * self;
-    static T IAlias<ILength>.Inject<T>(ISystems<ILength, T> basis) => basis.Imperial<Yard>();
+    static T ISystemInject<ILength>.Inject<T>(ISystems<ILength, T> basis) => basis.Imperial<Yard>();
     public static String Representation => "rōd";
 }

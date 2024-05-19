a602780e5c7b32b45a5404480ecd495b04b41abb
Thomas Kägi <kaegit@gmail.com>
2024-05-19 10:18:47 +0200

    Minor change.

--- source/Atmoos.Quantities.Units.Test/Usings.cs
+++ source/Atmoos.Quantities.Units.Test/Usings.cs
@@ -2,6 +2,7 @@
 global using Atmoos.Quantities.Units.Imperial.Length;
 global using Atmoos.Quantities.Units.Si;
 global using Xunit;
+
 global using static Atmoos.Quantities.Systems;
 global using static Atmoos.Quantities.TestTools.Convenience;
 global using static Atmoos.Quantities.TestTools.Traits;

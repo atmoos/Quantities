1fd56a2ecc7e9f4d0eace2e4e0eb857666798783
Thomas Kägi <kaegit@gmail.com>
2025-11-19 06:53:42 +0100

    Continue here

--- source/Atmoos.Quantities.Test/Serialization/QuantityFactoryTest.cs
+++ source/Atmoos.Quantities.Test/Serialization/QuantityFactoryTest.cs
@@ -1,13 +1,15 @@
 
 
+using Atmoos.Quantities.Dimensions;
 using Atmoos.Quantities.Serialization;
+using Atmoos.Quantities.Units;
 using Atmoos.Quantities.Units.Si.Metric;
 
 namespace Atmoos.Quantities.Test.Serialization;
 
 public class QuantityFactoryTest
 {
-    private static UnitRepository unitRepository = UnitRepository.Create();
+    private static readonly UnitRepository unitRepository = UnitRepository.Create();
 
     [Fact]
     public void ScalarQuantitiesCanBeBuilt()
@@ -47,5 +49,56 @@ public class QuantityFactoryTest
         var actual = QuantityFactory<Velocity>.Create(unitRepository, models).Build(expected.Value);
 
         Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    // The main point here is that the type verification still works.
+    public void CompoundQuantityCanBeBuiltUsingScalarValue()
+    {
+        var model = new QuantityModel {
+            System = "any",
+            Exponent = 1,
+            Unit = "not a knot"
+        };
+        var units = UnitRepository.Create([typeof(QuantityFactoryTest).Assembly]);
+        var expected = Velocity.Of(243.1792708715, NonStandard<Knot>());
+
+        var actual = QuantityFactory<Velocity>.Create(units, model).Build(expected.Value);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void NoAvailableUnitToPick()
+    {
+        var model = new QuantityModel {
+            System = "si",
+            Exponent = 1,
+            Unit = "something"
+        };
+
+        Assert.Throws<ArgumentException>(() => QuantityFactory<Velocity>.Create(unitRepository, model).Build(3));
+    }
+
+    [Fact]
+    public void WrongExponent()
+    {
+        var model = new QuantityModel {
+            System = "si",
+            Exponent = 2,
+            Unit = "m"
+        };
+
+        Assert.Throws<InvalidOperationException>(() => QuantityFactory<Velocity>.Create(unitRepository, model).Build(3));
+    }
+
+    public readonly struct Knot : INonStandardUnit, IVelocity
+    {
+        public static String Representation => "not a knot";
+        public static Transformation ToSi(Transformation self)
+        {
+            const Double notKnot = 2.193;
+            return notKnot * self;
+        }
     }
 }

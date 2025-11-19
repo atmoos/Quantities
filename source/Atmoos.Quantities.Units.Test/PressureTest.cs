30af0e52a6171be6c01d7f7b676eb00ff2e11a26
Thomas Kägi <kaegit@gmail.com>
2025-11-19 17:21:45 +0100

    Add minimal test set.

---
+++ source/Atmoos.Quantities.Units.Test/PressureTest.cs
@@ -0,0 +1,46 @@
+using Atmoos.Quantities.Units.Imperial.Force;
+using Atmoos.Quantities.Units.Si.Derived;
+using Atmoos.Quantities.Units.Si.Metric;
+
+namespace Atmoos.Quantities.Units.Test;
+
+public sealed class PressureTest
+{
+
+    [Fact]
+    public void HectoPascalToString() => FormattingMatches(p => Pressure.Of(p, Si<Hecto, Pascal>()), "hPa");
+    [Fact]
+    public void KiloNewtonPerSquareMetre() => FormattingMatches(p => Pressure.Of(p, Si<Kilo, Newton>().Per(Square(Si<Metre>()))), "kN/m²");
+    [Fact]
+    public void PoundForcePerSquareFoot() => FormattingMatches(p => Pressure.Of(p, Imperial<PoundForce>().Per(Square(Imperial<Foot>()))), "lbf/ft²");
+
+    [Fact]
+    public void PascalIsNewtonPerSquareMetre()
+    {
+        Pressure onePascal = Pressure.Of(1, Si<Pascal>());
+        Pressure siDefinition = Pressure.Of(1, Si<Newton>().Per(Square(Si<Metre>())));
+
+        Assert.Equal(onePascal, siDefinition);
+    }
+
+    [Fact]
+    public void BarConvertsToPascal()
+    {
+        Pressure pascal = Pressure.Of(1e5, Si<Pascal>());
+        Pressure oneBar = Pressure.Of(1, Metric<Bar>());
+
+        Assert.Equal(pascal, oneBar);
+    }
+
+    [Fact]
+    public void PsiIsConvertsToPascal()
+    {
+        const Double valueInPascals = 6894.75729316836133672267; // from WolframAlpha
+        Pressure pascals = Pressure.Of(valueInPascals, Si<Pascal>());
+        Pressure psi = Pressure.Of(1, Imperial<PoundForce>().Per(Square(Imperial<Inch>())));
+        Pressure convertedPsi = psi.To(Si<Pascal>());
+
+        Assert.Equal(pascals, psi);
+        convertedPsi.Matches(pascals);
+    }
+}
---
+++ source/Atmoos.Quantities.Units/Imperial/Force/PoundForce.cs
@@ -0,0 +1,10 @@
+using Atmoos.Quantities.Dimensions;
+using Atmoos.Quantities.Units.Si.Derived;
+
+namespace Atmoos.Quantities.Units.Imperial.Force;
+
+public readonly struct PoundForce : IImperialUnit, IForce
+{
+    public static Transformation ToSi(Transformation self) => 8896443230521 * self.RootedIn<Newton>() / 2000000000000;
+    public static String Representation => "lbf";
+}

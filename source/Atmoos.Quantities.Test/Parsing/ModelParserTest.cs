22aa818dae47ee548ed99ca3fa13737512cef795
Thomas Kägi <kaegit@gmail.com>
2025-11-30 10:54:04 +0100

    Add more model parser tests.

--- source/Atmoos.Quantities.Test/Parsing/ModelParserTest.cs
+++ source/Atmoos.Quantities.Test/Parsing/ModelParserTest.cs
@@ -22,6 +22,13 @@ public abstract class ModelParserTest
     protected static String Mul(String left, String right) => Join(left, right, '\u200C');
     protected static String Join(String left, String right, Char joiner) => $"{left}{joiner}{right}";
 
+    protected static TheoryData<TData> ToTheoryData<TData>(params TData[] data)
+        => data.Aggregate(new TheoryData<TData>(), (td, item) =>
+        {
+            td.Add(item);
+            return td;
+        });
+
     private protected static ModelParser CreateParser<TDimension>()
         where TDimension : IDimension
         => new(repository.Subset<TDimension>());
@@ -30,39 +37,39 @@ public abstract class ModelParserTest
     public sealed class ElectricCapacity : IProduct<IElectricCurrent, ITime> { }
 }
 
-public abstract class ScalarModelParserTest<TDimension> : ModelParserTest
+public abstract class ModelParserTest<TDimension> : ModelParserTest
     where TDimension : IDimension
 {
     private readonly ModelParser parser = CreateParser<TDimension>();
-    public abstract void ScalarValuesCanBeParsed(String unit, QuantityModel expected);
     protected void TestScalarParsing(String unit, QuantityModel expected)
     {
         var actual = this.parser.Parse(unit).ToArray();
 
         Assert.Equal([expected], actual);
     }
-}
-
-public abstract class CompoundModelParserTest<TDimension> : ModelParserTest
-    where TDimension : IDimension
-{
-    private readonly ModelParser parser = CreateParser<TDimension>();
-    public abstract void CompoundValuesCanBeParsed(String unit, IEnumerable<QuantityModel> expected);
     protected void TestCompoundParsing(String unit, IEnumerable<QuantityModel> expected)
     {
         var actual = this.parser.Parse(unit).ToArray();
 
         Assert.Equal(expected, actual);
     }
+    protected void ConfirmParsingFails(String unit)
+    {
+        var actual = this.parser.Parse(unit).ToArray();
+
+        Assert.Empty(actual);
+    }
 }
 
-public sealed class LengthParsingTest : ScalarModelParserTest<Length>
+public sealed class LengthParsingTest : ModelParserTest<Length>
 {
     [Theory]
-    [MemberData(nameof(ScalarLengths))]
-    public override void ScalarValuesCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
-
-    public static TheoryData<String, QuantityModel> ScalarLengths()
+    [MemberData(nameof(Lengths))]
+    public void LengthCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonLengths))]
+    public void NonLengthCannotBeParsed(String unit) => ConfirmParsingFails(unit);
+    public static TheoryData<String, QuantityModel> Lengths()
         => new() {
                 { "m", m },
                 { "ft", ft },
@@ -70,13 +77,17 @@ public sealed class LengthParsingTest : ScalarModelParserTest<Length>
                 { "mm", m with {Prefix = "m"}},
                 { "mi", mi },
             };
+    public static TheoryData<String> NonLengths() => ToTheoryData("mK", "s", "h", "kg");
 }
 
-public sealed class VolumeParsingTest : ScalarModelParserTest<Volume>
+public sealed class VolumeParsingTest : ModelParserTest<Volume>
 {
     [Theory]
     [MemberData(nameof(Volumes))]
-    public override void ScalarValuesCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    public void VolumeCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonVolumes))]
+    public void NonVolumeCannotBeParsed(String unit) => ConfirmParsingFails(unit);
     public static TheoryData<String, QuantityModel> Volumes()
         => new() {
                 { "m³", m with {Exponent = 3} },
@@ -84,13 +95,17 @@ public sealed class VolumeParsingTest : ScalarModelParserTest<Volume>
                 { "km³", km with {Exponent = 3} },
                 { "ℓ", new() { System = metric, Exponent = 1, Unit = "ℓ" } },
             };
+    public static TheoryData<String> NonVolumes() => ToTheoryData("Pa", "m/s", "kg", "°C");
 }
 
-public sealed class VelocityParsingTest : CompoundModelParserTest<Velocity>
+public sealed class VelocityParsingTest : ModelParserTest<Velocity>
 {
     [Theory]
     [MemberData(nameof(Velocities))]
-    public override void CompoundValuesCanBeParsed(String unit, IEnumerable<QuantityModel> expected) => TestCompoundParsing(unit, expected);
+    public void VelocityCanBeParsed(String unit, IEnumerable<QuantityModel> expected) => TestCompoundParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonVelocities))]
+    public void NonVelocityCannotBeParsed(String unit) => ConfirmParsingFails(unit);
     public static TheoryData<String, IEnumerable<QuantityModel>> Velocities()
         => new() {
                 { "m/s", [m, s with {Exponent = -1} ]},
@@ -100,51 +115,67 @@ public sealed class VelocityParsingTest : CompoundModelParserTest<Velocity>
                 { "km/h", [km, h with {Exponent = -1}]},
                 { "mi/h", [mi, h with {Exponent = -1}]}
             };
+    public static TheoryData<String> NonVelocities() => ToTheoryData("kg", "°C", "A", "μK");
 }
 
-public sealed class MassParsingTest : ScalarModelParserTest<Mass>
+public sealed class MassParsingTest : ModelParserTest<Mass>
 {
     [Theory]
     [MemberData(nameof(Masses))]
-    public override void ScalarValuesCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    public void MassCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonMasses))]
+    public void NonMassCannotBeParsed(String unit) => ConfirmParsingFails(unit);
     public static TheoryData<String, QuantityModel> Masses()
         => new() {
                 { "kg", new() { System = si, Exponent =1, Unit = "kg" } },
                 { "lb", new() { System = imperial, Exponent =1, Unit = "lb"} }
             };
+    public static TheoryData<String> NonMasses() => ToTheoryData("J", "mK", "s", "°C", "m/s");
 }
 
-public sealed class TimeParsingTest : ScalarModelParserTest<Time>
+public sealed class TimeParsingTest : ModelParserTest<Time>
 {
     [Theory]
     [MemberData(nameof(Times))]
-    public override void ScalarValuesCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    public void TimeCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonTimes))]
+    public void NonTimeCannotBeParsed(String unit) => ConfirmParsingFails(unit);
     public static TheoryData<String, QuantityModel> Times()
         => new() {
                 { "s", s },
                 { "μs", μs },
                 { "h", h }
             };
+    public static TheoryData<String> NonTimes() => ToTheoryData("kg", "°C", "mol", "pt", "N");
 }
 
-public sealed class TemperatureParsingTest : ScalarModelParserTest<Temperature>
+public sealed class TemperatureParsingTest : ModelParserTest<Temperature>
 {
     [Theory]
     [MemberData(nameof(Temperatures))]
-    public override void ScalarValuesCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    public void TemperatureCanBeParsed(String unit, QuantityModel expected) => TestScalarParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonTemperatures))]
+    public void NonTemperatureCannotBeParsed(String unit) => ConfirmParsingFails(unit);
     public static TheoryData<String, QuantityModel> Temperatures()
         => new() {
                 { "°C", new() { System = metric, Exponent = 1, Unit = "°C" } },
                 { "K", new() { System = si, Exponent = 1, Unit = "K" } },
                 { "mK", new() { System = si, Exponent = 1, Unit = "K" , Prefix = "m"} }
             };
+    public static TheoryData<String> NonTemperatures() => ToTheoryData("mol", "m", "s", "kg", "m/s", "°C/s", "°C·s", "m/s²");
 }
 
-public sealed class AccelerationParsingTest : CompoundModelParserTest<Acceleration>
+public sealed class AccelerationParsingTest : ModelParserTest<Acceleration>
 {
     [Theory]
     [MemberData(nameof(Accelerations))]
-    public override void CompoundValuesCanBeParsed(String unit, IEnumerable<QuantityModel> expected) => TestCompoundParsing(unit, expected);
+    public void AccelerationCanBeParsed(String unit, IEnumerable<QuantityModel> expected) => TestCompoundParsing(unit, expected);
+    [Theory]
+    [MemberData(nameof(NonAccelerations))]
+    public void NonAccelerationCannotBeParsed(String unit) => ConfirmParsingFails(unit);
     public static TheoryData<String, IEnumerable<QuantityModel>> Accelerations()
         => new() {
                 { "m/s²", [m, s with {Exponent = -2} ]},
@@ -152,13 +183,16 @@ public sealed class AccelerationParsingTest : CompoundModelParserTest<Accelerati
                 { "ft/μs²", [ft, s with {Exponent = -2, Prefix = "μ" } ]},
                 { "mi/h²", [mi, h with {Exponent = -2} ]},
             };
+    // ToDo: At the moment the exponent is entirely passive. Hence, "s", "m/s³" & "m/s" can all be parsed here.
+    //       Consider letting this fail here by checking exponents in the parser.
+    public static TheoryData<String> NonAccelerations() => ToTheoryData("kg", "K", "pt", "mK");
 }
 
-public sealed class ElectricCapacityParsingTest : CompoundModelParserTest<ModelParserTest.ElectricCapacity>
+public sealed class ElectricCapacityParsingTest : ModelParserTest<ModelParserTest.ElectricCapacity>
 {
     [Theory]
     [MemberData(nameof(ElectricCapacities))]
-    public override void CompoundValuesCanBeParsed(String unit, IEnumerable<QuantityModel> expected) => TestCompoundParsing(unit, expected);
+    public void ElectricCapacityCanBeParsed(String unit, IEnumerable<QuantityModel> expected) => TestCompoundParsing(unit, expected);
     public static TheoryData<String, IEnumerable<QuantityModel>> ElectricCapacities()
         => new() {
                 { Mul("A","h"), [new() { System = si, Exponent = 1, Unit = "A" }, h ]},

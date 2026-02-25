e361f1f8c7c4ba736539d58ef4d8be9e878b754c
Thomas Kägi <kaegit@gmail.com>
2026-02-25 21:25:56 +0100

    Remove superflous ToSi method in Hertz

--- source/Atmoos.Quantities.Units/Si/Derived/Hertz.cs
+++ source/Atmoos.Quantities.Units/Si/Derived/Hertz.cs
@@ -4,8 +4,6 @@ namespace Atmoos.Quantities.Units.Si.Derived;
 
 public readonly struct Hertz : ISiUnit, IFrequency, IInvertible<ITime>
 {
-    public static Transformation ToSi(Transformation self) => self;
-
     static T ISystemInject<ITime>.Inject<T>(ISystems<ITime, T> basis) => basis.Si<Second>();
 
     public static String Representation => "Hz";

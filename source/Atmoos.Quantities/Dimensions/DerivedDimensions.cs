7c80524610baf68b6aa67bed2bb75dcc656e76d2
Thomas Kägi <kaegit@gmail.com>
2026-03-17 07:00:01 +0100

    Add dimensionless dimension interface

--- source/Atmoos.Quantities/Dimensions/DerivedDimensions.cs
+++ source/Atmoos.Quantities/Dimensions/DerivedDimensions.cs
@@ -16,8 +16,6 @@ public interface IPower : ILinear<IPower>, IDerivedQuantity; // marker interface
 
 public interface IEnergy : IProduct<IPower, ITime>, IDerivedQuantity; // marker interface
 
-public interface IAngle : IProduct<ILength, IDimension<ILength, Negative<One>>>, IDerivedQuantity; // marker interface
-
 public interface IFrequency : IDimension<ITime, Negative<One>>, ILinear, IDerivedQuantity; // marker interface
 
 public interface IPressure : IProduct<IForce, IDimension<ILength, Negative<Two>>>, IDerivedQuantity; // marker interface
---
+++ source/Atmoos.Quantities/Dimensions/Dimensionless.cs
@@ -0,0 +1,10 @@
+﻿namespace Atmoos.Quantities.Dimensions;
+
+public interface IDimensionless<TSelf> : IDimension
+    where TSelf : IDimensionless<TSelf>
+{
+    static Dimension IDimension.D { get; } = Unit.Identity;
+}
+
+// By SI definition, angle is dimensionless and a derived quantity: https://en.wikipedia.org/wiki/SI_derived_unit
+public interface IAngle : IDimensionless<IAngle>, IDerivedQuantity; // marker interface

54e04f0c60f4be329eb61432fa16fee59939fc13
Thomas Kägi <kaegit@gmail.com>
2026-08-18 21:22:35 +0200

    Add quotient creator tests

--- source/Atmoos.Quantities.Test/Creation/CreatorsTest.ai.cs
+++ source/Atmoos.Quantities.Test/Creation/CreatorsTest.ai.cs
@@ -66,4 +66,69 @@ public class CreatorsTest
         converted.Matches(Quantity.Of<Product<Si<Metre>, Power<Si<Second>, Negative<One>>>>(7_200_000));
         chained.Transform(converted).Matches(actual);
     }
+
+    /*
+    {Product<Product<Si<Kilo, Si.Metre>, Power<Metric<Si.Metric.Minute>, Negative<One>>>, Si<Si.Second>>}
+    */
+
+
+    [Fact]
+    public void QuotientTimesScalarMeasurePreservesTermsAndConversionFactor()
+    {
+        var quotient = Si<Kilo, Metre>().Per(Metric<Minute>());
+        var chained = quotient.Times(Si<Second>());
+        var si = Si<Metre>().Per(Metric<Hour>()).Times(Si<Second>());
+
+        Quantity actual = chained.Create(2);
+        actual.Matches(Quantity.Of<Product<Product<Si<Kilo, Metre>, Power<Metric<Minute>, Negative<One>>>, Si<Second>>>(2));
+
+        Quantity converted = si.Transform(actual);
+        converted.Matches(Quantity.Of<Product<Product<Si<Metre>, Power<Metric<Hour>, Negative<One>>>, Si<Second>>>(120_000));
+        chained.Transform(converted).Matches(actual);
+    }
+
+    [Fact]
+    public void QuotientTimesPoweredMeasurePreservesTermsAndConversionFactor()
+    {
+        var quotient = Si<Kilo, Metre>().Per(Metric<Minute>());
+        var chained = quotient.Times(Square(Metric<Minute>()));
+        var si = Si<Metre>().Per(Metric<Hour>()).Times(Square(Si<Second>()));
+
+        Quantity actual = chained.Create(2);
+        actual.Matches(Quantity.Of<Product<Product<Si<Kilo, Metre>, Power<Metric<Minute>, Negative<One>>>, Power<Metric<Minute>, Two>>>(2));
+
+        Quantity converted = si.Transform(actual);
+        converted.Matches(Quantity.Of<Product<Product<Si<Metre>, Power<Metric<Hour>, Negative<One>>>, Power<Si<Second>, Two>>>(432_000_000));
+        chained.Transform(converted).Matches(actual);
+    }
+
+    [Fact]
+    public void QuotientPerScalarMeasurePreservesTermsAndConversionFactor()
+    {
+        var quotient = Si<Kilo, Metre>().Per(Metric<Minute>());
+        var chained = quotient.Per(Si<Second>());
+        var si = Si<Metre>().Per(Metric<Hour>()).Per(Si<Second>());
+
+        Quantity actual = chained.Create(2);
+        actual.Matches(Quantity.Of<Product<Product<Si<Kilo, Metre>, Power<Metric<Minute>, Negative<One>>>, Power<Si<Second>, Negative<One>>>>(2));
+
+        Quantity converted = si.Transform(actual);
+        converted.Matches(Quantity.Of<Product<Product<Si<Metre>, Power<Metric<Hour>, Negative<One>>>, Power<Si<Second>, Negative<One>>>>(120_000));
+        chained.Transform(converted).Matches(actual);
+    }
+
+    [Fact]
+    public void QuotientPerPoweredMeasurePreservesTermsAndConversionFactor()
+    {
+        var quotient = Si<Kilo, Metre>().Per(Metric<Minute>());
+        var chained = quotient.Per(Square(Si<Second>()));
+        var si = Si<Metre>().Per(Metric<Hour>()).Per(Square(Metric<Minute>()));
+
+        Quantity actual = chained.Create(2);
+        actual.Matches(Quantity.Of<Product<Product<Si<Kilo, Metre>, Power<Metric<Minute>, Negative<One>>>, Power<Si<Second>, Negative<Two>>>>(2));
+
+        Quantity converted = si.Transform(actual);
+        converted.Matches(Quantity.Of<Product<Product<Si<Metre>, Power<Metric<Hour>, Negative<One>>>, Power<Metric<Minute>, Negative<Two>>>>(432_000_000));
+        chained.Transform(converted).Matches(actual);
+    }
 }
